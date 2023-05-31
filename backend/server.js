0b66d5be109149caccd41d243a2adf9bf7278665
Tiago Gomes <tidgomes@gmail.com>
2023-05-31 16:43:19 -0300

    feat: configure dotenv to be used and set the port for the server

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 import express from 'express'
+import dotenv from 'dotenv'
 
+dotenv.config()
+const PORT = process.env.PORT || 8080
 const app = express()
 
 app.use((req, res) => {
   res.status(200).json({ message: 'Hello There Friend!' })
 })
 
-app.listen(5000, () => {
-  console.log(`Hello, server up and running`)
+app.listen(PORT, () => {
+  console.log(`Hello, server up and running on port ${PORT}`)
 })
