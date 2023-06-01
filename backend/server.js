770e96364c74cfabae0d930ed91561f0a741e705
Tiago Gomes <tidgomes@gmail.com>
2023-06-01 10:27:17 -0300

    feat: handle request to get all movies

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,16 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import connectToDB from './config/db.js'
+import Movie from './model/Movie.js'
 
 dotenv.config()
 const PORT = process.env.PORT || 8080
 const app = express()
 connectToDB()
 
-app.use((req, res) => {
-  res.status(200).json({ message: 'Hello There Friend!' })
+app.use('/', async (req, res) => {
+  const movies = await Movie.find()
+  res.status(200).json(movies)
 })
 
 app.listen(PORT, () => {
