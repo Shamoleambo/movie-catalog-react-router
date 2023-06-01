70e037e597acca09ec3c7bc1150c74ea27ded49c
Tiago Gomes <tidgomes@gmail.com>
2023-06-01 11:22:42 -0300

    feat: set up a router and use it in the RouterProvider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import RootLayout from './routes/RootLayout'
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
